58e8d0739d1dc93f533574581172e3b52b3bc95b
Dublonx <dublonjmx17@gmail.com>
2026-04-13 17:52:17 +0800

    update

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // Calculator
+const display = document.querySelector('#calculatorDisplay');
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => {
+ button.addEventListener("click", () => {
+  const value = button.innerText;
+  
+  // if
+  if (value === "AC") {
+   display.value = "";
+  } else if (value === "R") {
+   display.value = display.value.slice(0, -1);
+  } else if (value === "=") {
+   display.value = eval(display.value);
+  } else {
+   display.value += value;
+  }
+ });
+});
